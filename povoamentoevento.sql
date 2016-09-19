cdf8bbdd2da6ca06a6d922f1c4a01b9615e514ee
Moabe Renato <mrov@cin.ufpe.br>
2016-09-19 15:28:28 -0300

    Um arquivo separado com os inserts da tabela Event
    Um arquivo separado com os inserts da tabela Event

---
+++ povoamentoevento.sql
@@ -0,0 +1,14 @@
+-- 2.16  EVENTO
+
+INSERT ALL
+INTO EVENTO VALUES('Professor imprensa' , '234908724-88', TO_DATE('07/09/2016', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '171615142-21', TO_DATE('07/10/2014', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '881391402-21', TO_DATE('16/07/2015', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '238432464-99', TO_DATE('10/02/2012', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '635752445-80', TO_DATE('14/09/2013', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '234908724-88', TO_DATE('23/06/2014', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '893451348-30', TO_DATE('07/09/2016', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '234908724-88', TO_DATE('08/01/2011', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '893451348-30', TO_DATE('08/12/2008', 'dd/MM/yyyy'))
+INTO EVENTO VALUES('Professor imprensa' , '353234979-23', TO_DATE('08/10/2009', 'dd/MM/yyyy'))
+SELECT 1 FROM DUAL;
